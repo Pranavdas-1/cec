2df9fd52b27d8ced2541f73b45d46ad0d2d30e7a
Pranav Das <138027582+Pranavdas-1@users.noreply.github.com>
2024-03-11 21:29:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var i = 0;
-var time = 3000;
+var time = 5000;
 var data = [
     ["Jyothirmayi Devi C", 'DSC_0426 4.png','Assistant Professor - Computer  Science Department','M Tech. in Computer Science & Engineering (2002-2004)','B Tech in Computer Science & Engineering (1986-1991)','jyothirmayi@ceconline.edu','304','Life time Member of “The Indian Society for Technical Education (ISTE)” an organization for promoting the quality and standards in Technical Education.','Coordinator of SUMMIT-2016, National tech fest conducted at College of Enginering , Chengannur.','Staff -in-charge of FOCES','1'],
-    ["Princy Sugathan", 'DSC_0434 1.png','Assistant Professor - Computer  Science Department','M Tech. In Software Engineering (2010-2012)','B Tech in Computer Engineering (1998-2002)',' ','304','Life time Member of “The Indian Society for Technical Education (ISTE)” an organization for promoting the quality and standards in Technical Education.','Additional Supt. Of Examination Cell for both Cochin University of Science and Technology and A P J Abdul Kalam Technological University.','Assistant Warden for Ladies Hostel of College of Engineering Chengannur.','1'],
+    ["Princy Sugathan", 'DSC_0434 1.png','Assistant Professor - Computer  Science Department','M Tech. In Software Engineering (2010-2012)','B Tech in Computer Engineering (1998-2002)','-','304','Life time Member of “The Indian Society for Technical Education (ISTE)” an organization for promoting the quality and standards in Technical Education.','Additional Supt. Of Examination Cell for both Cochin University of Science and Technology and A P J Abdul Kalam Technological University.','Assistant Warden for Ladies Hostel of College of Engineering Chengannur.','1'],
     ['Betty James', 'DSC_0314 (1) 1.png','Assistant Professor - Computer  Science Department','M.Tech. in Digital Image Processing','BTech in Computer Science and Engineering.','bettycec@ceconline.edu','304',' ',' ',' ','0'],
     ['Shemeema Hashim', 'photo-Shemeema-Hasihim 1.png','Assistant Professor - Computer  Science Department','B.Tech in Computer Science and Engineering','M. Tech in Computer and Information Science','shemeemahashim786@gmail.com','304',' ',' ',' ','0']
 ];
@@ -28,7 +28,7 @@ function changeContent() {
 
     i++;
     if (i >= data.length) {
-        i = 0;
+        window.location.href = "index2.html";
     }
 
     setTimeout(changeContent, time);
